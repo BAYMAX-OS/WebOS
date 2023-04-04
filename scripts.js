68239ffc36b4e36b34105442641c9a1c0ba8f53b
Ryland Mihalcin <76697058+rylandishere@users.noreply.github.com>
2023-04-03 22:04:19 -0400

    New update

--- scripts.js
+++ scripts.js
@@ -94,7 +94,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const credits = document.getElementById('credits')
   const by = document.createElement('p')
   by.innerHTML =
-    'You are not running the latest version of BAYMAX OS! Please reload this tab.'
+    'Running BAYMAX WebOS 0.2.5 Stable Build.'
   credits.appendChild(by)
   credits.style.opacity = '0.6'
   document.getElementById('overlay').style.opacity = '0.8'
